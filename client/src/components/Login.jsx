ee2f7cca041f50c8b31e3b1503e981d56a7c8aed
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2023-04-19 21:25:03 +0100

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,18 @@
 import { Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import { auth, provider, provider2 } from '../firebase'
+import { signInWithPopup } from 'firebase/auth'
 
 export default function Login({ closeLogin }) {
   const [open, setOpen] = useState(true)
 
   const cancelButtonRef = useRef(null)
+  const signInWithGoogle = async () => {
+    signInWithPopup(auth, provider).then((result) => {
+      console.log(result)
+    })
+  }
 
   return (
     <Transition.Root
@@ -32,7 +39,7 @@ export default function Login({ closeLogin }) {
         </Transition.Child>
 
         <div className='fixed inset-0 z-10 overflow-y-auto'>
-          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+          <div className='flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0'>
             <Transition.Child
               as={Fragment}
               enter='ease-out duration-300'
@@ -47,7 +54,7 @@ export default function Login({ closeLogin }) {
                   <button
                     type='button'
                     className=' w-1/2 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'
-                    // onClick={() => setOpen(false)}
+                    onClick={signInWithGoogle}
                   >
                     Google Login
                   </button>
